e635cfcd8d2c047f48f29165d91473068676627d
YazanSufian99 <yazan.sufian@yahoo.com>
2022-03-08 22:02:55 +0300

    edit files content

--- Netflix-Clone/src/component/MovieList.jsx
+++ Netflix-Clone/src/component/MovieList.jsx
@@ -1,38 +1,16 @@
-
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import ModalMovie from './ModalMovie';
 import {useState} from 'react';
+import Movie from './Movie';
 let MovieList = ({ movies }) => {
   
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-
+ 
   return (
     <>
       {
         movies.map(movies => {
           return (
-            <div key={movies.id}>
-              <Card style={{ width: '18rem', padding:"10px ", margin:"15px " }}>
-                <Card.Img variant="top" src={movies.poster_path} />
-                <Card.Body>
-                  <Card.Title style={{ color: 'lightblue' }}>{movies.id}</Card.Title>
-                  <Card.Text style={{ fontFamily: 'cursive', letterSpacing: '1px' }}>
-                    {movies.overview}.
-                  </Card.Text>
-                  <Card.Subtitle>
-                    {movies.release_date}
-                  </Card.Subtitle>
-                  
-                  <Button variant="primary" onClick={handleShow}>Add to favorite</Button>
-                </Card.Body>
-              </Card>
+            <div  key={movies.id}>
 
-              <ModalMovie movies={movies} show={show} handleClose={handleClose} />
+              <Movie movies={movies}/>
             </div>
           )
         })
